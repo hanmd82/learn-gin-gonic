00aa5a04e4fe9c3ab70b28a2b4d791446cdbd896
Mingding, Han <hanmd82@gmail.com>
2020-08-06 23:20:26 +0800

    feat: Add login page and setup admin route group

--- main.go
+++ main.go
@@ -12,5 +12,14 @@ func main() {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
+	r.GET("/login", func (c *gin.Context){
+		c.HTML(http.StatusOK, "login.html", nil)
+	})
+
+	admin := r.Group("/admin")
+	admin.GET("/", func (c *gin.Context){
+		c.HTML(http.StatusOK, "admin-overview.html", nil)
+	})
+
 	r.Run(":3000")
 }
