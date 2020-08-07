5a8a4fccb3cc5b971abdb8e235adc89557b39f7d
Mingding, Han <hanmd82@gmail.com>
2020-08-07 21:53:13 +0800

    refactor: standardise formatting and dos2unix line-endings

--- src/app/controller.go
+++ src/app/controller.go
@@ -13,7 +13,7 @@ func registerRoutes() *gin.Engine {
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	r.GET("/login", func (c *gin.Context){
+	r.GET("/login", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "login.html", nil)
 	})
 
@@ -27,23 +27,23 @@ func registerRoutes() *gin.Engine {
 		}
 
 		c.HTML(http.StatusOK, "vacation-overview.html",
-			map[string]interface{} {
+			map[string]interface{}{
 				"TimesOff": timesOff,
-		})
+			})
 	})
 
 	admin := r.Group("/admin", gin.BasicAuth(gin.Accounts{
 		"admin": "admin",
 	}))
 
-	admin.GET("/", func (c *gin.Context){
+	admin.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "admin-overview.html",
-			map[string]interface{} {
+			map[string]interface{}{
 				"Employees": employees,
-		})
+			})
 	})
 
-	admin.GET("/employees/:id", func (c *gin.Context) {
+	admin.GET("/employees/:id", func(c *gin.Context) {
 		id := c.Param("id")
 		if id == "add" {
 			c.HTML(http.StatusOK, "admin-employee-add.html", nil)
@@ -57,9 +57,9 @@ func registerRoutes() *gin.Engine {
 		}
 
 		c.HTML(http.StatusOK, "admin-employee-edit.html",
-			map[string]interface{} {
+			map[string]interface{}{
 				"Employee": employee,
-		})
+			})
 	})
 
 	r.Static("/public", "./public")
