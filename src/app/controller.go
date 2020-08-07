6e25a9a37cd679d9f2e7d140068fabdca1dbf1a9
Mingding, Han <hanmd82@gmail.com>
2020-08-07 16:54:16 +0800

    feat: Add parameterized routes for employee vacations

--- src/app/controller.go
+++ src/app/controller.go
@@ -17,6 +17,21 @@ func registerRoutes() *gin.Engine {
 		c.HTML(http.StatusOK, "login.html", nil)
 	})
 
+	r.GET("/employees/:id/vacation", func(c *gin.Context) {
+		id := c.Param("id")
+		timesOff, ok := TimesOff[id]
+
+		if !ok {
+			c.String(http.StatusNotFound, "404 - Page Not Found")
+			return
+		}
+
+		c.HTML(http.StatusOK, "vacation-overview.html",
+			map[string]interface{} {
+				"TimesOff": timesOff,
+		})
+	})
+
 	admin := r.Group("/admin")
 	admin.GET("/", func (c *gin.Context){
 		c.HTML(http.StatusOK, "admin-overview.html", nil)
